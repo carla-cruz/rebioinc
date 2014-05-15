14a3e2f5e82ecdf141046999175a8976f565c368
Paulette Luftig <pyluftig@gmail.com>
2014-05-14 19:35:40 -0700

    add simple password lock to site

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  http_basic_authenticate_with :name => "bioenergy", :password => "cabernet"
   
   def index
     @team_members = TeamMember.all
