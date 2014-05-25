8b03b19e81526ebd5ab3d652ca614da47b055639
Paulette Luftig <pyluftig@gmail.com>
2014-05-24 19:00:07 -0700

    changing the feel

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,17 +1,17 @@
 
-$(document).ready(function(){
-  $window = $(window);
-  $('div[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-        $(window).scroll(function() {
-    // Scroll the background at var speed
-    // the yPos is a negative value because we're scrolling it UP!
-    var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-    // Put together our final background position
-    var coords = '50% '+ yPos + 'px';
-    // Move the background
-    $bgobj.css({ backgroundPosition: coords });
-        }); // window scroll Ends
-      });
+// $(document).ready(function(){
+//   $window = $(window);
+//   $('div[data-type="background"]').each(function(){
+//         var $bgobj = $(this); // assigning the object
+//         $(window).scroll(function() {
+//     // Scroll the background at var speed
+//     // the yPos is a negative value because we're scrolling it UP!
+//     var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+//     // Put together our final background position
+//     var coords = '50% '+ yPos + 'px';
+//     // Move the background
+//     $bgobj.css({ backgroundPosition: coords });
+//         }); // window scroll Ends
+//       });
 
-});
+// });
