e03b189f78ea1f07481d749fa467d03e454592ec
Paulette Luftig <pyluftig@gmail.com>
2014-05-26 08:59:28 -0700

    adding an our process page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rebioinc::Application.routes.draw do
   root :to => "static_pages#index"
   get '/admin' => 'static_pages#admin'
   get '/philosophy' => 'static_pages#philosophy'
+  get '/our_process' => 'static_pages#our_process'
 
 end
