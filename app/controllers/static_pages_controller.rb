fd8f08c462755204b8c4a190464d1face505f1f2
Paulette Luftig <pyluftig@gmail.com>
2014-05-27 12:22:41 -0700

    change seed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   http_basic_authenticate_with :name => "bioenergy", :password => "cabernet"
 
-  before_filter :is_admin?, except: [:index]
+  before_filter :is_admin?, except: [:index, :our_process_2]
   
   def index
     @team_members = TeamMember.all
@@ -12,6 +12,9 @@ class StaticPagesController < ApplicationController
     @team_members = TeamMember.all
     @faqs = Faq.all
     @hs = HomeStatement.all
+  end
+
+  def our_process_2
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rebioinc::Application.routes.draw do
   get '/admin' => 'static_pages#admin'
   get '/philosophy' => 'static_pages#philosophy'
   get '/our_process' => 'static_pages#our_process'
+  get '/our_process_2' => 'static_pages#our_process_2'
 
   resources :team_members
   resources :faqs
