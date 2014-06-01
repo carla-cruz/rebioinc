bedfe2443cf128bb015617e55d217d7314d5bc90
Paulette Luftig <pyluftig@gmail.com>
2014-06-01 08:22:38 -0700

    trying to get a=back to the old

--- app/assets/javascripts/our_process.js
+++ app/assets/javascripts/our_process.js
@@ -1,90 +1,76 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// $('.our-process-container img').hover(ShowToolTip);
-//  });
+  $('.cross').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+  });
 
-// var imgId;
-// ShowToolTip = function(){
+  $('#1').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip1').toggle();
+  });
 
-// imgId = $(this).attr('id') 
+  $('#2').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip2').toggle();
+  });
 
-//  switch(imgId){
-//     case 1:
-//     alert ('did it')
-//     break;
-// }
+  $('#3').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip3').toggle();
+  });
 
+  $('#4').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip4').toggle();
+  });
 
-  // $('.cross').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  // });
+  $('#5').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip5').toggle();
+  });
 
-  // $('#1').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip1').toggle();
-  // });
+  $('#6').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip6').toggle();
+  });
 
-  // $('#2').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip2').toggle();
-  // });
+  $('#7').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip7').toggle();
+  });
 
-  // $('#3').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip3').toggle();
-  // });
+  $('#8').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip8').toggle();
+  });
 
-  // $('#4').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip4').toggle();
-  // });
+  $('#9').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip9').toggle();
+  });
 
-  // $('#5').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip5').toggle();
-  // });
+  $('#10').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip10').toggle();
+  });
 
-  // $('#6').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip6').toggle();
-  // });
-
-  // $('#7').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip7').toggle();
-  // });
-
-  // $('#8').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip8').toggle();
-  // });
-
-  // $('#9').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip9').toggle();
-  // });
-
-  // $('#10').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip10').toggle();
-  // });
-
-  // $('#11').on('click', function(e){
-  //   e.preventDefault();
-  //   $('.outerTip').hide();
-  //   $('.tip11').toggle();
-  // });
+  $('#11').on('click', function(e){
+    e.preventDefault();
+    $('.outerTip').hide();
+    $('.tip11').toggle();
+  });
+});
 
 
 
