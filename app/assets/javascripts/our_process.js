d5d0c095aa1887a60f7f1f0f248878711a66cf5c
Carla Cruz <carla13cruz@gmail.com>
2014-06-01 11:30:50 -0700

    styled sliced model image

--- app/assets/javascripts/our_process.js
+++ app/assets/javascripts/our_process.js
@@ -1,4 +1,71 @@
+// <script>
+//   $(document).ready(function(){
+//   $('.cross').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//   });
 
+//   $('#1').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip1').toggle();
+//   });
+
+//   $('#2').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip2').toggle();
+//   });
+
+//   $('#3').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip3').toggle();
+//   });
+
+//   $('#4').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip4').toggle();
+//   });
+
+//   $('#5').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip5').toggle();
+//   });
+
+//   $('#6').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip6').toggle();
+//   });
+
+//   $('#7').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip7').toggle();
+//   });
+
+//   $('#8').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip8').toggle();
+//   });
+
+//   $('#9').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip9').toggle();
+//   });
+
+//   $('#10').on('click', function(e){
+//     e.preventDefault();
+//     $('.outerTip').hide();
+//     $('.tip10').toggle();
+//   });
+// });
+// </script>
 
 
 
